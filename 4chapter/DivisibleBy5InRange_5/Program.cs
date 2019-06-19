02528fcb72def99cb55b4a83b422b8b3e31dd7b6
thicckWire <gurdeepgss@outlook.com>
2019-06-19 16:19:09 +0530

    compute numbers that are divisible by 5 in a specified range

---
+++ 4chapter/DivisibleBy5InRange_5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DivisibleBy5InRange_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Numbers in Range (a, b) divisible by 5");
+
+			Console.Write("Enter lower bound of the range: ");
+			int lowerBound = int.Parse(Console.ReadLine());
+
+			Console.Write("Enter upper bound of the range: ");
+			int upperBound = int.Parse(Console.ReadLine());
+
+			string divisibleBy5 = "";
+			int counter = 0;
+			for(int index = lowerBound; index <= upperBound; index++)
+			{
+				if(index % 5 == 0)
+				{
+					counter++;
+					divisibleBy5 = divisibleBy5 + " " + index;
+				}
+			}
+
+			Console.WriteLine($"There are {counter} numbers divisible by 5 in range ({lowerBound}, {upperBound}).");
+			Console.WriteLine($"The numbers are {divisibleBy5}.");
+        }
+    }
+}
