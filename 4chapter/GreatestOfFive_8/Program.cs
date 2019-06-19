160b1ca784e1ba4309a22f3cb65bb09ef7a01ee4
thicckWire <gurdeepgss@outlook.com>
2019-06-19 17:49:46 +0530

    prints greatest among five numbers without array or any data-structure

---
+++ 4chapter/GreatestOfFive_8/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace GreatestOfFive_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Greatest of the Five Numbers");
+
+			Console.Write("Enter first number:");
+			double firstNumber = double.Parse(Console.ReadLine());
+
+			Console.Write("Enter second number:");
+			double secondNumber = double.Parse(Console.ReadLine());
+
+			Console.Write("Enter third number:");
+			double thirdNumber = double.Parse(Console.ReadLine());
+
+			Console.Write("Enter fourth number:");
+			double fourthNumber = double.Parse(Console.ReadLine());
+
+			Console.Write("Enter fifth number:");
+			double fifthNumber = double.Parse(Console.ReadLine());
+
+			string all = $"{firstNumber} {secondNumber} {thirdNumber} {fourthNumber} {fifthNumber}";
+
+			for(int index = 0; index < 2; index++)
+			{
+				firstNumber = (firstNumber > secondNumber)? firstNumber : secondNumber;
+				if(index == 1)
+				{
+					fourthNumber = firstNumber;
+					fifthNumber = thirdNumber;
+				}
+				secondNumber = (fourthNumber > fifthNumber)? fourthNumber : fifthNumber;
+			}
+
+			Console.WriteLine($"Greatest among {all} is {secondNumber}.");
+        }
+    }
+}
