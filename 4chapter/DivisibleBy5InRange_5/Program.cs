9eca684fdfc6325a2269bf3c23cbd7726ce1a1e0
thicckWire <gurdeepgss@outlook.com>
2019-06-19 18:10:22 +0530

    added book's implementation

--- 4chapter/DivisibleBy5InRange_5/Program.cs
+++ 4chapter/DivisibleBy5InRange_5/Program.cs
@@ -15,16 +15,38 @@ namespace DivisibleBy5InRange_5
 			int upperBound = int.Parse(Console.ReadLine());
 
 			string divisibleBy5 = "";
+			int index = lowerBound;
 			int counter = 0;
-			for(int index = lowerBound; index <= upperBound; index++)
+
+			/* One of the implementations from Book */
+			while(true)
 			{
 				if(index % 5 == 0)
-				{
-					counter++;
-					divisibleBy5 = divisibleBy5 + " " + index;
-				}
+					break;
+				index++;
 			}
-
+			
+			/*
+			 * using the fact that if a number is divisible by 5 then every 5th number from it
+			 * is also divisible by 5 
+			 */
+			while(index <= upperBound)
+			{
+				counter++;
+				divisibleBy5 = divisibleBy5 + " " + index;
+				index += 5;
+			}
+			/*
+			 * My implementation
+			 *	for(int index = lowerBound; index <= upperBound; index++)
+			 *	{
+			 *		if(index % 5 == 0)
+			 *		{
+			 *			counter++;
+			 *			divisibleBy5 = divisibleBy5 + " " + index;
+			 *		}
+			 *	}
+			 */
 			Console.WriteLine($"There are {counter} numbers divisible by 5 in range ({lowerBound}, {upperBound}).");
 			Console.WriteLine($"The numbers are {divisibleBy5}.");
         }
