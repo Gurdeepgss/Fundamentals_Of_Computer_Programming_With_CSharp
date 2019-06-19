795a3671b8b89b30201afa0b75d266e8a15d5df9
thicckWire <gurdeepgss@outlook.com>
2019-06-19 17:19:55 +0530

    prints greater number using ternary operator

---
+++ 4chapter/PrintGreaterWithTwist_6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PrintGreaterWithTwist_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			/*
+			 * This program is to be done without using conditional statements
+			 */
+            Console.WriteLine("Find which number is greater");
+
+			Console.Write("Enter first number: ");
+			double firstNubmer = double.Parse(Console.ReadLine());
+			Console.Write("Enter second number: ");
+			double secondNumber = double.Parse(Console.ReadLine());
+
+			double greater = (firstNubmer > secondNumber)? firstNubmer : secondNumber;
+			double lesser = (firstNubmer + secondNumber) - greater;
+
+			Console.WriteLine($"{greater} is greater than {lesser}.");
+        }
+    }
+}
