586882f2f132195b597fb14a0ea826d90313088f
thicckWire <gurdeepgss@outlook.com>
2019-06-19 13:39:23 +0530

    get radius, print perimeter and area of circle

---
+++ 4chapter/PerimeterAndAreaOfCircle_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PerimeterAndAreaOfCircle_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculate Perimeter and Area of Circle");
+
+			Console.Write("Enter 1 for Area or 2 for Perimeter of Circle: ");
+			int choice = int.Parse(Console.ReadLine());
+
+			if(choice == 1 || choice == 2)
+			{
+				Console.Write("Enter radius of Circle: ");
+				float radius = float.Parse(Console.ReadLine());
+
+				string measure;
+				float result;
+
+				if(choice == 1)
+				{
+					measure = "Area";
+					result = (float)(Math.PI * Math.Pow(radius, 2));
+				}
+				else
+				{
+					measure = "Perimeter";
+					result = (float)(2 * Math.PI * radius);
+				}
+
+				Console.WriteLine($"The {measure} of Circle with radius {radius} is {result:F2}.");
+			}
+			else
+			{
+				Console.WriteLine($"Choice {choice} is not valid.");
+			}
+        }
+    }
+}
