aa18c5259d2d1a4e303f4d89ba4b91d3a8b03fe5
thicckWire <gurdeepgss@outlook.com>
2019-06-20 07:44:36 +0530

    Prints first n natural numbers

---
+++ 4chapter/print_n_naturalNumbers_10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace print_n_naturalNumbers_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prints sequence \"n\" natural numbers");
+			Console.Write("Enter the last natural number of the sequence: ");
+			long lastNaturalNumbers = long.Parse(Console.ReadLine());
+
+			for(int index = 0; index < lastNaturalNumbers; index++)
+			{
+				Console.WriteLine($"{index + 1, 2}");
+			}
+
+        }
+    }
+}
