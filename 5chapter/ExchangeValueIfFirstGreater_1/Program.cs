ea289f0501fa07dc317c4380c155efa42a941671
thicckWire <gurdeepgss@outlook.com>
2019-06-20 13:01:54 +0530

    exchange values if first number is greater

---
+++ 5chapter/ExchangeValueIfFirstGreater_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ExchangeValueIfFirstGreater_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program to exchange values of first number is greater");
+
+			Console.Write("Enter first number: ");
+			double firstNumber = double.Parse(Console.ReadLine());
+			Console.Write("Enter second number: ");
+			double secondNumber = double.Parse(Console.ReadLine());
+
+			Console.WriteLine($"first number is: {firstNumber}.");
+			Console.WriteLine($"second number is: {secondNumber}.");
+
+			if(firstNumber > secondNumber)
+			{
+				double temp = secondNumber;
+				secondNumber = firstNumber;
+				firstNumber = temp;
+				Console.WriteLine("\nThe excahanged Values are listed below");
+				Console.WriteLine($"first number is: {firstNumber}.");
+				Console.WriteLine($"second number is: {secondNumber}.");
+			}
+
+        }
+    }
+}
