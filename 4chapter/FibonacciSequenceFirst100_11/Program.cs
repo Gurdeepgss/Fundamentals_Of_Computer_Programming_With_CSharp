0e0a6b5d635fd52c90777bd2633eae52220601d4
thicckWire <gurdeepgss@outlook.com>
2019-06-20 08:16:38 +0530

    Fibonacci sequence, first 100 numbers

---
+++ 4chapter/FibonacciSequenceFirst100_11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FibonacciSequenceFirst100_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("First 100 numbers in Fibonacci sequence: ");
+
+			double present = 1;
+			double previous = 0;
+			double sum = 0;
+
+			Console.WriteLine($"{previous,2}\n{present,2}");
+
+			for(int index = 0; index < 100; index++)
+			{
+				sum = present + previous;
+				previous = present;
+				present = sum;
+
+				Console.WriteLine($"{index + 1,3}. {sum, 2}");
+			}
+        }
+    }
+}
