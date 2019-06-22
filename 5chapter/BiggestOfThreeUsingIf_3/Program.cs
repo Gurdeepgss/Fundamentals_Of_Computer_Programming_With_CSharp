ecad5aeb594503c9485ff1dbba1d6cd183a553de
thicckWire <gurdeepgss@outlook.com>
2019-06-22 09:21:16 +0530

    biggest of three integers using if statements

---
+++ 5chapter/BiggestOfThreeUsingIf_3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace BiggestOfThreeUsingIf_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find the Biggest among three numbers");
+
+			/* User Input the numbers */
+			Console.Write("Enter number 1: ");
+			double firstNumber = double.Parse(Console.ReadLine());
+			Console.Write("Enter number 2: ");
+			double secondNumber = double.Parse(Console.ReadLine());
+			Console.Write("Enter number 3: ");
+			double thirdNumber = double.Parse(Console.ReadLine());
+
+			/* Compute the biggest using if statements */
+			double biggest = double.MinValue;
+			if(firstNumber > secondNumber)
+			{
+				if(firstNumber > thirdNumber)
+				{
+					biggest = firstNumber;
+				}
+				else
+				{
+					biggest = thirdNumber;
+				}
+			}
+			else
+			{
+				if(secondNumber > thirdNumber)
+				{
+					biggest = secondNumber;
+				}
+				else
+				{
+					biggest = thirdNumber;
+				}
+			}
+
+			/* Print out the result */
+			Console.WriteLine($"The Biggest number among {firstNumber}, {secondNumber}, {thirdNumber} is: {biggest}");
+        }
+    }
+}
