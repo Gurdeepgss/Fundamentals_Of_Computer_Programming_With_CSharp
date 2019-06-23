4880006a5d948f2f4e0e5137746f787d8a2de74c
thicckWire <gurdeepgss@outlook.com>
2019-06-23 08:56:52 +0530

    print first 'n' natural numbers

---
+++ 6chapter/NaturalNumbers_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NaturalNumbers_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Print first \"n\" Narutal numbers");
+
+			Console.Write("Enter n: ");
+			long n = long.Parse(Console.ReadLine());
+
+			for(long index = 1; index <= n; index++)
+			{
+				if((index - 1) % 10 == 0)
+				{
+					Console.WriteLine();
+				}
+				Console.Write("\t" + index);
+			}
+
+			Console.WriteLine();
+        }
+    }
+}
