827e861eecbf8e758d8aa0f0f7bd72ce8dc2545b
thicckWire <gurdeepgss@outlook.com>
2019-06-23 19:14:52 +0530

    matrix pattern 1 2 3,\n 2 3 4,\n 3 4 5.

---
+++ 6chapter/MatrixPatternSizeN_10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MatrixPatternSizeN_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Matrix pattern");
+
+			for(int row = 0; row < 3; row++)
+			{
+				for(int column = row; column < row + 3; column++)
+				{
+					Console.Write($"{column + 1, 3}");
+				}
+				Console.WriteLine();
+			}
+        }
+    }
+}
