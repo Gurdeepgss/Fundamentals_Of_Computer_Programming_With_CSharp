5584d2348221eac9f51cdec9d7d3881252cbf0ca
thicckWire <gurdeepgss@outlook.com>
2019-06-23 12:55:39 +0530

    computation involving factorial, use BigInteger for really BIG numbers

---
+++ 6chapter/InvolvingFactorial_7/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace InvolvingFactorial_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			/*
+			 * Since factorials become very large for some numbers, therfore it is recommended
+			 * to use BigInteger data-type for this purposes as often calculations result into
+			 * infinity.
+			 */
+            Console.WriteLine("Compute (N! * K!) / (N - K)! (where 1 < K < N)");
+
+			Console.Write("Enter n: ");
+			long n = long.Parse(Console.ReadLine());
+			Console.Write("Enter k: ");
+			long k = long.Parse(Console.ReadLine());
+
+			double kFactorial = 1;
+			double nFactorial = 1;
+			double nMinusKFactorial = 1;
+			for(int index = 1; index <= n; index++)
+			{
+				nFactorial *= index;
+
+				if(index == k)
+				{
+					kFactorial = nFactorial;
+				}
+				if(index == (n - k))
+				{
+					nMinusKFactorial = nFactorial;
+				}
+			}
+
+			Console.WriteLine($"{n}! = {nFactorial}, {k}! = {kFactorial}.");
+
+			double result = (nFactorial * kFactorial) / nMinusKFactorial;
+
+			Console.WriteLine($"({n}! * {k}!) / ({n} - {k})! is: {result}.");
+        }
+    }
+}
