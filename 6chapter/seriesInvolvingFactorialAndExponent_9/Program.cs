8031fe535983b11fcdb901c8339375220d3dc48f
thicckWire <gurdeepgss@outlook.com>
2019-06-23 17:23:09 +0530

    sum of series involving factorial and exponential terms

---
+++ 6chapter/seriesInvolvingFactorialAndExponent_9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace seriesInvolvingFactorialAndExponent_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string specialSeries = " 0!/x\u2070 + 1!/x\u00b9 + 2!/x\u00b2 + 3!/x\u00b3 + ... + n!/x\u207f";
+            Console.WriteLine($"Sum of Special series {specialSeries}");
+
+			Console.Write("Enter x: ");
+			int x = int.Parse(Console.ReadLine());
+			Console.Write("Enter n: ");
+			int n = int.Parse(Console.ReadLine());
+
+			double sumOfSpecialSeries = 1;
+			double factorial = 1;
+			double exponentialTerm = 0;
+			specialSeries = "1/1";
+			for(int index = 1; index <= n; index++)
+			{
+				factorial *= index;
+				exponentialTerm = Math.Pow(x, index);
+				specialSeries += $" + {factorial}/{exponentialTerm}";
+				sumOfSpecialSeries += factorial / exponentialTerm;
+			}
+
+			Console.WriteLine($"{specialSeries} = {sumOfSpecialSeries}.");
+        }
+    }
+}
