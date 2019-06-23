f10aa8f9b8a0228e4a8d049fee5ea2b995b42723
thicckWire <gurdeepgss@outlook.com>
2019-06-23 12:33:20 +0530

    compute division of n! by k!

---
+++ 6chapter/FactorialDivision_6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FactorialDivision_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Compute n!/k! (where 1<k<n)");
+
+			Console.Write("Enter n: ");
+			long n = long.Parse(Console.ReadLine());
+			Console.Write("Enter k: ");
+			long k = long.Parse(Console.ReadLine());
+
+			double kfactorial = 1;
+			double nfactorial = 1;
+			for(int index = 1; index <= n; index++)
+			{
+				nfactorial *= index;
+
+				if(index == k)
+				{
+					kfactorial = nfactorial;
+				}
+			}
+
+			Console.WriteLine($"{n}! = {nfactorial}, {k}! = {kfactorial}.");
+
+			double factorialDivision = nfactorial / kfactorial;
+
+			Console.WriteLine($"{n}! / {k}! is: {factorialDivision}.");
+        }
+    }
+}
