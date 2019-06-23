5782a668caeaa5745004fe256c9c0dfb204551e8
thicckWire <gurdeepgss@outlook.com>
2019-06-23 10:21:38 +0530

    print smallest and largest numbers from user input

---
+++ 6chapter/SmallestAndLargest_3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SmallestAndLargest_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Compute Smallest and Largest number among input");
+
+			double largest = double.MinValue;
+			double smallest = double.MaxValue;
+			while(true)
+			{
+				Console.Write("Enter a number or q to quit: ");
+				string userInput = Console.ReadLine();
+
+				if(userInput == "q")
+				{
+					break;
+				}
+
+				double userNumber = double.Parse(userInput);
+
+				if(userNumber > largest)
+				{
+					largest = userNumber;
+				}
+				if(userNumber < smallest)
+				{
+					smallest = userNumber;
+				}
+			}
+
+			Console.WriteLine($"The Smallest number is: {smallest}.");
+			Console.WriteLine($"The Largest number is: {largest}.");
+        }
+    }
+}
