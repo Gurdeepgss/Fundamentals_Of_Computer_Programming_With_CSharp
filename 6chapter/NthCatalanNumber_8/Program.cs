038e01d14ebae48bb1eae8fba2cafb045b2480e0
thicckWire <gurdeepgss@outlook.com>
2019-06-23 16:31:48 +0530

    compute catalan numbers, they involve factorial

---
+++ 6chapter/NthCatalanNumber_8/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NthCatalanNumber_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Get Nth Catalan number i.e. (2N)! / ((N + 1)! * N!) where N >= 0");
+
+			Console.Write("Enter N: ");
+			double n = double.Parse(Console.ReadLine());
+
+			double twoNFactorial = 1;
+			double nFactorial = 1;
+			double nPlusOneFactorial = 1;
+			for(int index = 1; index <= 2 * n; index++)
+			{
+				twoNFactorial *= index;
+
+				if(index == n)
+				{
+					nFactorial = twoNFactorial;
+				}
+				if(index == (n + 1))
+				{
+					nPlusOneFactorial = twoNFactorial;
+				}
+			}
+
+			double nthCatalanNumber = twoNFactorial / (nPlusOneFactorial * nFactorial);
+
+			Console.WriteLine($"Catalan number for N = {n} i.e. (2 * {n})! / (({n} + 1)! * {n}!) is: {nthCatalanNumber}.");
+        }
+    }
+}
