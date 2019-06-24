9b5f244c785cffaa707d6236df55a880df9e77d2
thicckWire <gurdeepgss@outlook.com>
2019-06-24 10:23:30 +0530

    reverse string by converting to char array and back to string

---
+++ OtherPrograms/ReverseString/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Get your String reversed");
+
+			Console.Write("Enter string: ");
+			string toBeReversed = Console.ReadLine();
+
+			char[] inCharToBeReversed = toBeReversed.ToCharArray();
+			Array.Reverse(inCharToBeReversed);
+			string reversed = new string(inCharToBeReversed);
+			
+			Console.WriteLine($"\"{toBeReversed}\" reversed is \"{reversed}\".");
+        }
+    }
+}
